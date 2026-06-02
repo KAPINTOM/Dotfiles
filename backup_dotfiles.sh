5c0b82455eba037276125355a0e11b5a46003eec
Kenneth Andrey Pinto Medina <kapintomedina@gmail.com>
2026-06-01 20:26:27 -0500

    Update Dotfiles

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -2,8 +2,11 @@
 # Backup dotfiles to ~/Dotfiles repository folder
 
 cp -R ~/.config/mpv .config
-
+cp -R ~/.config/fish .config
 cp -R ~/.config/qtile .config
+cp -R ~/.config/alacritty .config
+cp -R ~/.config/kitty .config
+cp -R ~/.config/picom .config
 
 rm -r ~/Dotfiles/.config/qtile/backup
 rm -r ~/Dotfiles/.config/qtile/__pycache__
