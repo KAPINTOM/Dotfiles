641d8b2b46e8d77e693eaab0e064504fac6bbcda
KAPINTOM <kapintomedina@gmail.com>
2026-07-05 12:05:35 -0500

    Update backup script and add xdg-desktop-portal configuration: include xdg-desktop-portal in backup and create portals.conf with preferred settings

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -9,6 +9,7 @@ cp -R ~/.config/kitty .config
 cp -R ~/.config/picom .config
 cp -R ~/.config/i3 .config
 cp -R ~/.config/polybar .config
+cp -R ~/.config/xdg-desktop-portal .config
 
 rm -r ~/Dotfiles/.config/qtile/backup
 rm -r ~/Dotfiles/.config/qtile/__pycache__
